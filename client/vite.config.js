77b16205eb34ec07023da330f89d07e42e241d04
Ed Ali <edali.personal@gmail.com>
2025-11-03 00:11:16 -0800

    fix: moving .env point to within client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
-dotenv.config({ path: '../.env' });
-const port = process.env.CLIENT_PORT || 5173;
+dotenv.config({ path: './.env' });
+const port = process.env.VITE_PORT || 5173;
 const serverTarget = `${process.env.VITE_API_URL || 'http://localhost:3001'}`;
 
 // https://vitejs.dev/config/
